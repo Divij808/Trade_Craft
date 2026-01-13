ba8ddf45a2ac015bc864850c3e30dde72581cdab
Divij808 <mekala.divij@gmail.com>
2026-01-13 15:30:12 +0000

    This code will create a cursor to create the database. As seen in my code I will name this Trade Craft. I have put these under a function, so that I can all the functions later to use in the main project(app.py).

--- database_setup.py
+++ database_setup.py
@@ -1,1 +1,8 @@
 import sqlite3
+
+def create_db(db_name="tradecraft.db"):
+    with sqlite3.connect(db_name) as conn:
+        _create_tables(conn)
+
+def create_db_connection(conn):
+    _create_tables(conn)
