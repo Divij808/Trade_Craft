d8aa1b7cf8e5008e7b620335c41d848943d07872
Divij808 <mekala.divij@gmail.com>
2026-01-13 18:28:21 +0000

    The following update was because, i was facing the error of "Users table does not have cash."
    This meant that the user would not be able to trade in this application without having virtual money to spend. Hence, added the cash.
    
    Further, this caused a syntax error where I had forgotten to add the comma between each line in the SQL statements. As result this has been fixed as seen.

--- create_db.py
+++ create_db.py
@@ -1,11 +1,14 @@
 import sqlite3
 
+
 def create_db(db_name="tradecraft.db"):
     with sqlite3.connect(db_name) as conn:
         _create_tables(conn)
 
+
 def create_db_connection(conn):
     _create_tables(conn)
+
 
 def _create_tables(conn):
     sql_statements = [
