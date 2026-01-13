16110ed4ac6e2c5db558657f7c90cf3e8a159498
Divij808 <mekala.divij@gmail.com>
2026-01-13 16:25:33 +0000

    This code asks the user in the python console for the user's username and password, to log in the application. This code, then inserts the user entered data into a database. There is a use of try and except statement as the database is set to unique. This means that if the user enters a name that already exists the database will provide  a sensible output and the same when creating the user account.

--- app.py
+++ app.py
@@ -1,7 +1,24 @@
+import sqlite3
+
 from create_db import create_db
 from werkzeug.security import generate_password_hash, check_password_hash
 create_db()
+DB_NAME = "tradecraft.db"
 def signup_user():
     username = input("Enter username: ")
     password = input("Enter password: ")
     password_hashed = generate_password_hash(password)
+
+    try:
+        with sqlite3.connect(DB_NAME) as conn:
+            cur = conn.cursor()
+            cur.execute(
+                "INSERT INTO users (username, password_hash, cash) VALUES (?, ?, ?)",
+                (username, generate_password_hash(password), 10000.0)
+            )
+            conn.commit()
+            print("✅ User created successfully!")
+    except sqlite3.IntegrityError:
+        print("❌ Username already exists.")
+
+
