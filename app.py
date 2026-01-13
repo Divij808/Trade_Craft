9dd2d5d2d5eecd26e2712b303df97099c5a0915d
Divij808 <mekala.divij@gmail.com>
2026-01-13 16:10:41 +0000

    As seen, I have created a non-ui terminal based project form the first prototype of this login page. T

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
 from create_db import create_db
 from werkzeug.security import generate_password_hash, check_password_hash
 create_db()
+def signup_user():
+    username = input("Enter username: ")
+    password = input("Enter password: ")
+    password_hash = generate_password_hash(password)
