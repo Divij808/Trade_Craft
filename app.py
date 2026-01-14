e5fc13e4e37f28d0bc8bb11dda9971b4f2ffb6a2
Divij808 <mekala.divij@gmail.com>
2026-01-14 21:47:00 +0000

    I have now indented the try and except statements. This is important because it allows the code to be able to access within the try and except statements to see the username and password

--- app.py
+++ app.py
@@ -16,25 +16,26 @@ def home():
     return "TradeCraft Running"
 
 
+
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
     if request.method == "POST":
         username = request.form["username"]
         password = request.form["password"]
-    try:
-        conn = sqlite3.connect("tradecrafts.db")
-        cursor = conn.cursor()
-        cursor.execute(
-            "INSERT INTO users (username, password_hash,cash) VALUES (?, ?, ?)",
-            (username, generate_password_hash(password), 10000.0)
-        )
-        conn.commit()
-        conn.close()
+        try:
+            conn = sqlite3.connect("tradecrafts.db")
+            cursor = conn.cursor()
+            cursor.execute(
+                "INSERT INTO users (username, password_hash,cash) VALUES (?, ?, ?)",
+                (username, generate_password_hash(password), 10000.0)
+            )
+            conn.commit()
+            conn.close()
 
-        return "User created"
-    except:
+            return "User created"
+        except:
 
-        print("Error creating user because there is already a user with that name")
+            print("Error creating user because there is already a user with that name")
 
     return render_template("signup.html")
 
