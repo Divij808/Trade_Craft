386f0b730cca2dd6b991e5297152bbebaa192f7d
Divij808 <mekala.divij@gmail.com>
2026-01-14 21:55:00 +0000

    The code now has a new function for login. The login has allowed the user to enter username and password and selects the password based on the username and compares the users password with the inputted.
    Since, the password has been hashed by werkzeug, it will also be decrypted and compared by werkzeug as seen with the use of the function check_password_hash.

--- app.py
+++ app.py
@@ -16,6 +16,26 @@ def home():
     return "TradeCraft Running"
 
 
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        conn = sqlite3.connect("tradecrafts.db")
+        cursor = conn.cursor()
+        cursor.execute(
+            "SELECT password_hash FROM users WHERE username=?", (username,)
+        )
+        result = cursor.fetchone()
+        conn.close()
+
+        if result and check_password_hash(result[0], password):
+            return "Login successful"
+        else:
+            return "Invalid credentials"
+    return render_template("login.html")
+
 
 @app.route("/signup", methods=["GET", "POST"])
 def signup():
